8b26a023fe39cca04bd3cbfba7f015797b2889cf
Eugene Golovin <Yevgeny.Golovin@yandex.ru>
2022-02-20 23:59:18 +0300

    D7 HW

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -177,3 +177,11 @@ APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
 # если задача не выполняется за 25 секунд, то она автоматически снимается, можете поставить время побольше,
 # но как правило, это сильно бьёт по производительности сервера
 APSCHEDULER_RUN_NOW_TIMEOUT = 25  # Seconds
+
+# Настройки конфигурации Celery для исп. Redis
+
+CELERY_BROKER_URL = 'redis://:Wzjcwh2Ifj4U5kKEETTZJpFOC7L9SzDM@redis-14383.c263.us-east-1-2.ec2.cloud.redislabs.com:14383/0'
+CELERY_RESULT_BACKEND = 'redis://:Wzjcwh2Ifj4U5kKEETTZJpFOC7L9SzDM@redis-14383.c263.us-east-1-2.ec2.cloud.redislabs.com:14383/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
---
+++ NewsPaper/news/tasks.py
@@ -0,0 +1,58 @@
+from celery import shared_task
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from datetime import datetime, timedelta
+
+from .models import Post, Category
+
+
+@shared_task
+def send_sub_mail_after_post(instance_id):
+    instance = Post.objects.get(pk=instance_id)
+    # print(instance)
+    current_cats = instance.postCategory.all()
+    # print(Category.objects.filter(post__pk=instance_id))
+    subs_list = []
+    cat_list = []
+    for cat in current_cats:
+        cat_list.append(cat.categoryName)
+        subs_list.extend([user for user in cat.subscribers.all()])
+    # рассылка на все категории из новости
+    cat_mess = ",".join(cat_list)
+    for sub_user in subs_list:
+        html_content = render_to_string(
+            'news/news_update_mail.html', {'user': sub_user, 'cat_mess': cat_mess, 'post': instance})
+        # instance - только созданный экземпляр класса post
+        msg = EmailMultiAlternatives(
+            subject=f'Здравствуй, Новая статья из категории(-ий){cat_mess}',
+            from_email='projects-mail-sf@yandex.ru',
+            to=[sub_user.email]
+        )
+
+        msg.attach_alternative(html_content, 'text/html')
+        msg.send()
+
+
+@shared_task
+def send_mail_every_monday():
+    for category in Category.objects.all():
+        url = ''
+        cat_subscribers = category.subscribers.all()
+        week_news = Post.objects.filter(
+            creationDate__range=[datetime.now(tz=None) - timedelta(days=7), datetime.now(tz=None)],
+            postCategory=category
+        )
+
+        if week_news.exists():
+            for week_new in week_news:
+                url += f'{week_new.title} http://127.0.0.1:8000{week_new.get_absolute_url()}, \n'
+
+            for subscriber in cat_subscribers:
+                send_mail(
+                    subject=f'Дайджест новостей за неделю по категории {category.categoryName}!',
+                    message=f'Привет {subscriber.username}, дайджест новостей за неделю: {url}',
+                    from_email='projects-mail-sf@yandex.ru',
+                    recipient_list=[subscriber.email]
+                )
+
